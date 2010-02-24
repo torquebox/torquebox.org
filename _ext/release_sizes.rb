f8c01c0eb62201385edcfb5ceb4d240eef233b0d
Bob McWhirter <bob@fnokd.com>
2010-02-24 14:16:55 -0500

    Further refine site for AweStruct.

---
+++ _ext/release_sizes.rb
@@ -0,0 +1,25 @@
+require 'net/http'
+
+class ReleaseSizes
+  SERVER = 'repository.torquebox.org'
+
+  def execute(site)
+    Net::HTTP.start( SERVER, 80) do |http|
+      site.releases.each do |release|
+        if ( release.size.nil? )
+          release_path = "/maven2/releases/org/torquebox/torquebox-bin/#{release.version}/torquebox-bin-#{release.version}.zip" 
+          response = http.head( release_path )
+          b = response['content-length'] || ''
+          if ( ! b.empty? )
+            b = b.to_i
+            kb = b / 1024
+            mb = kb / 1024
+            release.size = mb
+          else
+            release.size = 'unknown'
+          end
+        end
+      end
+    end
+  end
+end
