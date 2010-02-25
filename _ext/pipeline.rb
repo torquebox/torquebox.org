571e4d61eeedc0373fd824509047ab7ffeb4f1c9
Bob McWhirter <bob@fnokd.com>
2010-02-25 13:57:00 -0500

    Rework to use Indexifier.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,7 @@
 require 'release_sizes'
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new( '/news' ) )
+  extension Awestruct::Extensions::Posts.new( 'news' ) 
   extension Awestruct::Extensions::Indexifier.new
   extension ReleaseSizes.new
 end
