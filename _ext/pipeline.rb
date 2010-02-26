82ad40e5451d131951a5b1e2d2767e9ae6eac082
Bob McWhirter <bob@fnokd.com>
2010-02-26 13:54:53 -0500

    Adding deployment script

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,7 +2,7 @@ require 'release_sizes'
 
 Awestruct::Extensions::Pipeline.new do
   extension ReleaseSizes.new
-  extension Awestruct::Extensions::Posts.new( 'news' ) 
+  extension Awestruct::Extensions::Posts.new( '/news' ) 
   extension Awestruct::Extensions::Indexifier.new
 end
 
