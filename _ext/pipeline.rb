8147d1d7a9e613656d5bfc0453b6e19466209630
Bob McWhirter <bob@fnokd.com>
2010-02-27 23:07:11 -0500

    Adjust for the Paginate extension in forthcoming Awestruct.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,7 +4,8 @@ require 'documentation'
 Awestruct::Extensions::Pipeline.new do
   extension Documentation.new()
   extension ReleaseSizes.new
-  extension Awestruct::Extensions::Posts.new( '/news' ) 
+  extension Awestruct::Extensions::Posts.new( '/news' )
+  extension Awestruct::Extensions::Paginate.new( :posts, '/news/index', :per_page=>3 )
   extension Awestruct::Extensions::Indexifier.new
 end
 
--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+rm -Rf _site _tmp
+../awestruct/bin/awestruct --url http://staging.torquebox.org/ --force
 rsync -rv --delete ./_site/ torquebox.org:/var/www/domains/torquebox.org/staging/htdocs/
