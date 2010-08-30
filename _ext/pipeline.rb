a5fe12c6c553e8cea814c8a3b20fe6acc733efaf
Bob McWhirter <bob@mcwhirter.org>
2010-08-30 10:28:32 -0400

    Adjust to newer Awestruct (0.0.8+).

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -9,8 +9,8 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page=>5 )
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new( :posts, '/news.atom' )
+  extension Awestruct::Extensions::IntenseDebate.new
 
-  helper Awestruct::Extensions::IntenseDebate
   helper Awestruct::Extensions::GoogleAnalytics
 
 end
