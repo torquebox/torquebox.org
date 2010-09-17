ea8c35e0e2c32680aaca6fa1803f60ad3484d294
Bob McWhirter <bob@mcwhirter.org>
2010-09-17 18:27:39 -0400

    Refactoring to use shared skin.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -7,13 +7,5 @@ Awestruct::Extensions::Pipeline.new do
   extension Documentation.new()
   extension Downloads.new()
   extension ReleaseSizes.new()
-  extension Awestruct::Extensions::Posts.new( '/news' )
-  extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page=>5 )
-  extension Awestruct::Extensions::Indexifier.new()
-  extension Awestruct::Extensions::Atomizer.new( :posts, '/news.atom' )
-  extension Awestruct::Extensions::IntenseDebate.new()
-
-  helper Awestruct::Extensions::GoogleAnalytics
-
 end
 
