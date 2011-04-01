d9b3320abdf7b1928ffc278be948d180ef34568f
Bob McWhirter <bob@mcwhirter.org>
2011-03-31 23:46:37 -0400

    We don't use those.

--- deploy-staging.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -Rf _site _tmp
-../awestruct/bin/awestruct --url http://staging.torquebox.org --force
-rsync -rvi --delete ./_site/ torquebox.org:/var/www/domains/torquebox.org/staging/htdocs/
