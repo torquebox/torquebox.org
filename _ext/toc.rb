d828dcc7b523483703ce9cd6ffc9d8b76ce27eec
Bob McWhirter <bob@mcwhirter.org>
2011-04-11 18:22:00 -0400

    Fix up out TOCer.

--- _ext/toc.rb
+++ _ext/toc.rb
@@ -12,9 +12,9 @@ class TOC
       toc = ""
 
       if page.is_a?(Awestruct::MarukuFile)
-        toc = parse_markdown_headers(page.raw_page_content.dup)
+        toc = parse_markdown_headers(page.raw_page_content)
       elsif page.is_a?(Awestruct::TextileFile)
-        toc = parse_textile_headers(page.raw_page_content.dup)
+        toc = parse_textile_headers(page.raw_page_content)
       end
 
       page.table_of_contents = toc
@@ -23,17 +23,39 @@ class TOC
   end
 
   def titleable(title)
-    title.strip.gsub(/\s/, "_").gsub(/\W/, "_").gsub(/_+$/, '')
+    title.strip.downcase.gsub(/\s+/, "_").gsub(/\W+/, "_").gsub(/_+/, '_').gsub(/_+$/, '')
   end
 
   def parse_markdown_headers(content)
     toc = ""
+    last_depth = 0
     content.gsub!(/^(\#{1,6})[ ]+(.+?)[ ]*\#*\n+/) do |match|
-      number = $1.size
+      number = $1.size.to_i
       name = $2.strip
       header = titleable(name)
-      toc << "#{' ' * number.to_i * 2}* [" + name + '](#' + header + ")\n" if number.to_i <= @options[:levels]
-      @options[:generate_header_links] ? "h#{number}. <a name=\"#{header}\" href=\"##{header}\">#{name}</a>\n" : "<h#{number} id=\"#{header}\">#{name}</h#{number}>\n"
+
+      if ( number > last_depth ) 
+        last_depth.upto( number -1) do |e|
+          toc << "<ul>"
+        end
+      elsif ( number.to_i < last_depth )
+        last_depth.downto( number +1 ) do |e|
+          toc << "</ul>"
+        end
+      end
+
+      toc << "<li><a href='##{header}'>#{name}</a></li>" 
+
+      last_depth = number
+
+      "<h#{number} id=\"#{header}\">#{name}</h#{number}>\n\n"
+    end
+
+    number = 1
+    if ( number.to_i < last_depth )
+      last_depth.downto( number ) do |e|
+        toc << "</ul>"
+      end
     end
 
     toc
