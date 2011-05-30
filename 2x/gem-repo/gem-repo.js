ed6ffe4881fd2562b44e45a465de43f719b56241
Bob McWhirter <bob@mcwhirter.org>
2011-05-29 20:54:12 -0400

    Adjust incremental builds to provide links to correct dist downloads and gem repos.

---
+++ 2x/gem-repo/gem-repo.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function() {
+  var handle_job_jsonp = function(data) {
+    //console.debug( data );
+    $( '#build_header' ).text( "Build " + data.number ); 
+    var gem_regexp = new RegExp( '^(.*).gem$' );
+    $.each( data.artifacts, function(i, artifact) {
+      if ( gem_regexp.exec( artifact.fileName ) ) {
+        console.debug( artifact );
+        $( '#gems' ).append( 
+          $( '<tr/>' ).append( 
+            $( '<td/>' ).append(
+              $( '<a href="./gems/' + artifact.fileName + '"/>' ).append(
+                artifact.fileName
+              )
+            )
+          )
+        );
+      }
+    } );
+  };
+
+  regexp = new RegExp( "^/2x/gem-repo/([0-9]+)/?$" );
+  console.debug( window.location );
+
+  match = regexp.exec( window.location.pathname );
+
+  var effective_version = "lastSuccessfulBuild";
+  if ( match ) {
+    effective_version = match[1];
+  }
+
+  console.debug( "Effective: " + effective_version );
+
+  
+  $.ajax( { 
+    url: 'https://torquebox.ci.cloudbees.com/job/torquebox-2x/label=m1.large,ruby_compat_version=1.8/' + effective_version + '/api/json?depth=1',
+    jsonp: 'jsonp',
+    dataType: 'jsonp',
+    type: 'GET',
+    context: self,
+    async: false,
+    success: handle_job_jsonp,
+  } );
+
+});
