9ca439b96cad4c94fbbac08c44e19d8da98203e5
Ben Browning <bbrownin@redhat.com>
2013-07-19 10:34:47 -0400

    Temporarily fix the IntenseDebate comment URLs until we update Awestruct

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -10,6 +10,37 @@ require 'tag_implier'
 require 'json'
 require 'podcasts'
 
+# HACK: only until this gets fixed upstream in Awestruct
+require 'digest/sha1'
+class IntenseDebateFixed
+  def execute(site)
+    site.pages.each{|p| p.extend IntenseDebatable }
+  end
+  module IntenseDebatable
+    def intense_debate_comments()
+      post_id = self.post_id ? self.post_id : Digest::SHA1.hexdigest( self.url )
+      html = %Q(<script>\n)
+      html += %Q(  var idcomments_acct='#{site.intense_debate_acct}';\n)
+      html += %Q(  var idcomments_post_id='#{post_id}';\n )
+      html += %Q(  var idcomments_post_url='#{site.intense_debate_base_url || site.base_url}#{self.url}';\n)
+      html += %Q(</script>\n)
+      html += %Q(<span id="IDCommentsPostTitle" style="display:none"></span>\n)
+      html += %Q(<script type='text/javascript' src='http://www.intensedebate.com/js/genericCommentWrapperV2.js'></script>\n)
+      html
+    end
+    def intense_debate_comments_link()
+      post_id = self.post_id ? self.post_id : Digest::SHA1.hexdigest( self.url )
+      html = %Q(<script>\n)
+      html += %Q(  var idcomments_acct='#{site.intense_debate_acct}';\n)
+      html += %Q(  var idcomments_post_id='#{post_id}';\n )
+      html += %Q(  var idcomments_post_url='#{site.intense_debate_base_url || site.base_url}#{self.url}';\n)
+      html += %Q(</script>\n)
+      html += %Q(<script type='text/javascript' src='http://www.intensedebate.com/js/genericLinkWrapperV2.js'></script>\n)
+      html
+    end
+  end
+end
+
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::DataDir.new
@@ -42,7 +73,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new(:posts, '/news.atom')
 
-  extension Awestruct::Extensions::IntenseDebate.new()
+  extension IntenseDebateFixed.new()
 
   extension Awestruct::Extensions::Tagger.new( :posts, 
                                                '/news/index', 
